03edce98d47bab12891305b3470c6e51b42a1705
Malikahnb <hasanboyevabudur@gmail.com>
2022-01-18 22:29:31 +0500

    updated

--- arithmetic.py
+++ arithmetic.py
@@ -2,7 +2,7 @@
 
 print('Solve the problem: 4 * 100 - 54: ')
 solution = 4 * 100 - 54
-user_solution = input('Enter your solution: ')
+user_solution = int(input('Enter your solution: '))
 
 if user_solution == solution:
     print(f'Your solution: {user_solution} \n'
