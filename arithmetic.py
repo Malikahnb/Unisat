9437c5ef591cc4da29d7f94091322267243c766c
Malikahnb <hasanboyevabudur@gmail.com>
2022-01-20 16:31:56 +0500

    fixed bool

--- arithmetic.py
+++ arithmetic.py
@@ -1,16 +1,18 @@
 # Задание 3.2
 
-print('Solve the problem: 4 * 100 - 54: ')
-solution = 4 * 100 - 54
-user_solution = int(input('Enter your solution: '))
-
-if user_solution == solution:
-    print(f'Your solution: {user_solution} \n'
-          f'Our solution: {solution}')
-else:
-    print('Incorrect. Try again.')
+# print('Solve the problem: 4 * 100 - 54: ')
+# solution = 4 * 100 - 54
+# user_solution = int(input('Enter your solution: '))
+#
+# if user_solution == solution:
+#     print(f'Your solution: {user_solution} \n'
+#           f'Our solution: {solution}')
+# else:
+#     print('Incorrect. Try again.')
 
 
 # --------------------------------------------------
 # Задание 3.3
-print(bool(False))
+print(type(False))
+
+# False равен к 0
